6e0e25b763a19874353ba7342d8f58ae3ff42626
Yoshihiro Sanada <yoshihiro.sanada@gmail.com>
2015-10-30 21:17:01 +0900

    商品登録機能の実装

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,5 +1,28 @@
 class GoodsController < ApplicationController
+  #ログインしていないユーザーが不正なアクセスをしない様に、予め設定しておく。
+  before_action :unlogin, except: [:index]
+
   def index
     @goods = Good.all
+  end
+
+  def new
+    @good = Good.new
+  end
+
+  def create
+    Good.create(create_params)
+    redirect_to :action => "index"
+  end
+
+  private
+  def create_params
+    params.require(:good).permit(:name, :stock_num, :nitification_num, :image, :amazon_url, :category_id)
+  end
+
+  def unlogin #ログインしていないユーザーの不正なアクセスに対してリダイレクトする。
+  unless user_signed_in?
+    redirect_to :action => "index"
+  end
   end
 end
