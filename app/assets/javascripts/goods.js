345a72585849e6288bf85e5ebd821da452c085cc
Yoshihiro Sanada <yoshihiro.sanada@gmail.com>
2015-12-05 12:33:06 +0900

    タブ切り替え機能の実装

--- app/assets/javascripts/goods.js
+++ app/assets/javascripts/goods.js
@@ -1,4 +1,5 @@
 $(function() {
+  // カウント機能の実装
   $('.count').on('click', function() {
     var get_id = $(this).attr("id");
     var id = "#good_" + get_id
@@ -9,4 +10,14 @@ $(function() {
     console.log(err)
   });
   });
+
+console.log("読み込まれました")
+  $('ul #list').on('click', function() {
+    // 一度currentを全て外す→thisにcurrentをあてる
+    $('ul #list').removeClass("active")
+    console.log("クラス解除!")
+    $(this).addClass("active")
+  });
+
+
 });
