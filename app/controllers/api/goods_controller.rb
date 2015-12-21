c03356913904e0799688df5e98a67279ad2e74b2
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-22 07:39:48 +0900

    define routeing/ define action

--- app/controllers/api/goods_controller.rb
+++ app/controllers/api/goods_controller.rb
@@ -1,2 +1,27 @@
 class Api::GoodsController < ApplicationController
+  protect_from_forgery expect: [:count_up, :count_down]
+
+  before_action :set_categories
+  before_action :set_goods
+
+  def count_up
+    @goods.update(stock_num: @goods.stock_num + 1)
+    @categories = Category.eager_load(:goods).all
+    render template: 'api/categories/index'
+  end
+
+  def count_down
+    @goods.update(stock_num: @goods.stock_num - 1)
+    @categories = Category.eager_load(:goods).all
+    render template: 'api/categories/index'
+  end
+
+  private
+  def set_categories
+    @categories = Category.eager_load(:goods).all
+  end
+
+  def set_goods
+    @goods = Good.find(params[:id])
+  end
 end
