c817fe30d767c64ae8f30b8f41afeeedcf2ab9f1
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-25 13:35:35 +0900

    create api

--- app/controllers/api/goods_controller.rb
+++ app/controllers/api/goods_controller.rb
@@ -1,5 +1,5 @@
 class Api::GoodsController < ApplicationController
-  protect_from_forgery expect: [:count_up, :count_down]
+  protect_from_forgery expect: [:count_up, :count_down, :edit]
 
   before_action :set_goods
 
@@ -13,8 +13,24 @@ class Api::GoodsController < ApplicationController
     render :show
   end
 
+  def update
+    @goods.update(update_params)
+    render :show
+  end
+
   private
   def set_goods
     @goods = Good.find(params[:id])
+  end
+
+  def update_params
+    params.permit(
+      :id,
+      :name,
+      :notification_num,
+      :stock_num,
+      :counting_type,
+      :category_id,
+    )
   end
 end
