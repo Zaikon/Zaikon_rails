aa886bd54ea76b0e564239c83295727db6bf5519
Yoshihiro Sanada <yoshihiro.sanada@gmail.com>
2016-12-28 16:36:45 +0900

    環境変数の定義

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,9 @@
 require File.expand_path('../application', __FILE__)
 
 Amazon::Ecs.options = {
-      associate_tag:     ENV["association_tag"],
-      AWS_access_key_id: ENV["AWS_access_key_id"],
-      AWS_secret_key:   ENV["AWS_secret_key"]
+      associate_tag:     ENV["ASSOCIATION_TAG"],
+      AWS_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      AWS_secret_key:   ENV["AWS_ACCESS_KEY_KEY"]
 }
 
 # Initialize the Rails application.
